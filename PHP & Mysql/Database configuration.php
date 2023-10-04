e703a747a296e0a1f5f55d78c7847da67b2d4225
RK Rukshan <44175424+rkrukshan@users.noreply.github.com>
2023-10-04 16:48:07 +0530

    Update Database configuration.php

--- PHP & Mysql/Database configuration.php
+++ PHP & Mysql/Database configuration.php
@@ -2,7 +2,7 @@
 $host_name = "localhost";
 $server_user_name = "root";
 $server_password = "";
-$db = "idms";
+$db = "db";
 
 $con = mysqli_connect($host_name, $server_user_name, $server_password, $db);
 
